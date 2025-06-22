e5c7bbe8a2e4daf511a7a6a51d2c38a1c8514245
Sami Warraich <samiwarraich51@gmail.com>
2025-06-22 18:57:11 +0300

    update DEFAULT_SKIP_IMG to use local image path

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const API_URL =
   "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft";
 
-export const DEFAULT_SKIP_IMG =
-  "https://yozbrydxdlcxghkphhtq.supabase.co/storage/v1/object/public/skips/skip-sizes/4-yarder-skip.jpg";
+export const DEFAULT_SKIP_IMG = "/skip.jpg";
+// "https://yozbrydxdlcxghkphhtq.supabase.co/storage/v1/object/public/skips/skip-sizes/4-yarder-skip.jpg";
